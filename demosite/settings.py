7aaef3ad774814a266e314969651addd32904969
Alex German <alex.germannn@gmail.com>
2016-10-20 17:03:05 +0300

    add mysql

--- demosite/settings.py
+++ demosite/settings.py
@@ -74,8 +74,10 @@ WSGI_APPLICATION = 'demosite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'python_db',
+        'USER': 'root',
+        'PASSWORD': '1'
     }
 }
 
